fe08e64dfc91bcf22f2978aca4f1dac1ae2c44e5
Igor Konovalov <konovalov.avp@gmail.com>
2017-05-01 21:36:49 +0300

    BOX

--- Box/index.js
+++ Box/index.js
@@ -1,10 +1,34 @@
 const nextCharFromNumberString = str => {
-  const trimmed = str.trim()
-  const number = parseInt(trimmed)
-  const nextNumber = number + 1
-  return String.fromCharCode(nextNumber)
+	const trimmed = str.trim()
+	const number = parseInt(trimmed)
+	const nextNumber = number + 1
+	return String.fromCharCode(nextNumber)
 }
 
-const result = nextCharFromNumberString(' 64')
+const nextCharFromNumberStringMap = str =>
+	[str]
+		.map(s => s.trim())
+		.map(n => new Number(n))
+		.map(n => n + 1)
+		.map(n => String.fromCharCode(n))
 
-console.log(result)
+const Box = x => ({
+	map: f => Box(f(x)),
+	inspect: () => `Box(f(x))`,
+  fold: f => f(x)
+})
+
+const nextCharFromNumberStringBox = str =>
+	Box(str)
+		.map(s => s.trim())
+		.map(n => new Number(n))
+		.map(n => n + 1)
+		.map(n => String.fromCharCode(n))
+    .fold(c  => c)
+
+console.log('1', nextCharFromNumberString(' 64'))
+
+console.log('2', nextCharFromNumberStringMap(' 64'))
+
+const result = nextCharFromNumberStringBox(' 64')
+console.log('3', result)
