10fed8016457f345630ef91ea75a92d46c059151
Igor Konovalov <konovalov.avp@gmail.com>
2017-05-14 11:15:06 +0300

    renaming for convinience, some Ether vs Imperative  examples

---
+++ EGGHEAD/ether/advanced_Ether_vs_Imperative.js
@@ -0,0 +1,24 @@
+const Right = x => ({
+  chain: f => f(x),
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`
+})
+
+const Left = x => ({
+  chain: f => Left(x),
+  map: f => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`
+})
+
+const tryCatch = f => {
+  try {
+    return Right(f())
+  } catch (e) {
+    return Left(e)
+  }
+}
+
+const fromNullable = x => (x !== null ? Right(x) : Left(null))
+
