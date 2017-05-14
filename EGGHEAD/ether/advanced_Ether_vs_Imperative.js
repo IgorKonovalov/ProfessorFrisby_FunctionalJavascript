24b290b1338c8afefd8629405cc6daf102821249
Igor Konovalov <konovalov.avp@gmail.com>
2017-05-14 11:20:08 +0300

    examples

--- EGGHEAD/ether/advanced_Ether_vs_Imperative.js
+++ EGGHEAD/ether/advanced_Ether_vs_Imperative.js
@@ -22,3 +22,94 @@ const tryCatch = f => {
 
 const fromNullable = x => (x !== null ? Right(x) : Left(null))
 
+/// EXAMPLES from https://egghead.io/lessons/javascript-a-collection-of-either-examples-compared-to-imperative-code
+
+const openSite = () => {
+  if (current_user) {
+    return renderPage(current_user)
+  } else {
+    return showLogin()
+  }
+}
+
+const openSiteF = () => fromNullable(current_user).fold(showLogin, renderPage)
+
+/////////////
+
+const getPrefs = user => {
+  if (user.premium) {
+    return loadPrefs(user.preferences)
+  } else {
+    return defaultPrefs
+  }
+}
+
+const getPrefsF = user =>
+  (user.premium ? Right(user) : Left('not premium'))
+    .map(u => u.preferences)
+    .fold(() => defaultPrefs, prefs => loadPrefs(prefs))
+
+/////////////
+
+const streetName = user => {
+  const address = user.address
+  if (address) {
+    const street = adress.street
+    if (street) {
+      return street.name
+    }
+  }
+  return 'no street'
+}
+
+const streetNameF = user =>
+  fromNullable(user.address)
+    .chain(a => fromNullable(a.street))
+    .map(s => s.name)
+    .fold(e => 'no street', n => n)
+
+/////////////
+
+const concatUniq = (x, ys) => {
+  const found = ys.filter(y => y === x)[0]
+  return found ? ys : ys.concat(x)
+}
+
+const concatUniqF = (x, ys) =>
+  fromNullable(ys.filter(y => y === x)[0]).fold(() => ys.concat(x), y => ys)
+
+/////////////
+
+const wrapExamples = example => {
+  if (example.previewPath) {
+    try {
+      example.preview = fs.readFileSync(example.previewPath)
+    } catch (e) {}
+  }
+  return example
+}
+
+const readFile = x => tryCatch(() => fs.readFileSync(x))
+
+const wrapExamplesF = example =>
+  fromNullable(example.previewPath)
+    .chain(readFile)
+    .fold(() => example, ex => Objects.assign({preview: p}, ex))
+
+/////////////
+
+const parseDbUrl = cfg => {
+  try {
+    const c = JSON.parse(cfg)
+    if (c.url) {
+      return c.url.match(/some pattern/)
+    }
+  } catch (e) {
+    return null
+  }
+}
+
+const parseDbUrlF = cfg =>
+  tryCatch(() => JSON.parse(cfg))
+    .chain(c => fromNullable(c.url))
+    .fold(e => null, u => u.match(/some pattern/))
